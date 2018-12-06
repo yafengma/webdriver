28e49e531886917ca5f5bcfaa3434a928bfa7634
yafeng <yafengma@webot.co>
2018-12-06 14:59:55 +0800

    增加异常

--- click.py
+++ click.py
@@ -2,6 +2,9 @@ import platform
 import time
 import sys
 import os
+
+from selenium.common.exceptions import TimeoutException, WebDriverException
+
 from webBrow import ProxyWebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -25,7 +28,12 @@ def run():
     br, wait_br = pw.get_driver()
     br.get(
         "http://admin.t7.site.webot.ai/static/h51/index.html?code=982ea76e92a211e787e1000c2913faa4&time=1543835171504")
-    wait_for(wait_br, By.ID, 'write').click()
+    while 1:
+        try:
+            wait_for(wait_br, By.ID, 'write').click()
+            break
+        except (TimeoutException, WebDriverException):
+            time.sleep(5)
     for i, msg1 in enumerate(msg1s):
         send_text(wait_br, 'write', msg1 + str(i))
         print(msg1 + str(i))
@@ -37,6 +45,7 @@ def run():
         send_text(wait_br, 'write', msg2 + str(i))
         print(msg2 + str(i))
         time.sleep(2)
+    br.close()
 
 
 def _server():
@@ -58,7 +67,7 @@ if __name__ == '__main__':
     else:
         kw = {
             'path': os.getcwd() + '/webdriver/windows/chromedriver.exe',
-            'headless': True
+            'headless': False
         }
     msg1s, msg2s = [['robot'] * 10, ['人工客服对话'] * 10]
 
